e07d79b47d1767a438a64cbbfc241feed548cb7e
Kartik Anand <kartikanand1992@gmail.com>
2015-04-11 20:17:03 +0530

    added support for logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
+import logging
+import sys
 from flask import Flask
 
 app = Flask(__name__)
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 from app import views
